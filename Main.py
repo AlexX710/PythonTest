a85f89fe39814ad50daf895c0241282e61472da3
Python Test <afloresvaz710@gmail.com>
2023-10-03 15:01:06 -0500

    Stream Test

--- Main.py
+++ Main.py
@@ -1,9 +1,9 @@
-print ("Today is a good day to learn Python")
-print ("Python is fun")
-print ("Python's string are easy to use")
-print ("hello" + " world")
+print("Today is a good day to learn Python")
+print("Python is fun")
+print("Python's string are easy to use")
+print("hello" + " world")
 greeting = "Hello"
-name - input ("PLease enter your name ")
-print (greeting + name)
+name = input("PLease enter your name ")
+print(greeting + name)
 # if we want a space, we can add that too
-print (greeting + '' + name)
+print(greeting + '' + name)
